610c9b405e6b532f0c78969d40d444a7cb6b0a61
NELLURI_KOUSHIK <koushiknelluri827@gmail.com>
2026-04-05 10:27:18 +0530

    modified routing and reqirements for chache updates

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,19 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from app.services.dns_service import get_ips
 from app.services.geo_service import get_location
 
 router = APIRouter()
 
 @router.get("/track")
-async def track(domain: str):
-    ips = get_ips(domain)
-    results = []
+async def track_domain(domain: str):
+    try:
+        ips = get_ips(domain)
 
-    for ip in ips:
-        location = await get_location(ip)
-        results.append({
-            "ip": ip,
-            "location": location
-        })
+        results = []
+        for ip in ips:
+            location = await get_location(ip)   # MUST await
+            results.append({
+                "ip": ip,
+                "location": location
+            })
 
-    return results
+        return results
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
