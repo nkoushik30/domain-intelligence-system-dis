2a9812c6658a943600d539a89e9f7aa8fb76730c
NELLURI_KOUSHIK <koushiknelluri827@gmail.com>
2026-07-18 18:16:36 +0530

    Add : Rate Limiting

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,13 +1,13 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 from app.services.dns_service import get_ips
 from app.services.geo_service import get_location
-from app.main import limiter
+from app.utils.rate_limiter import limiter
 
 router = APIRouter()
 
 @router.get("/track")
 @limiter.limit("10/minute")
-async def track_domain(domain: str):
+async def track_domain(request: Request, domain: str):
     try:
         ips = get_ips(domain)
 
--- app/main.py
+++ app/main.py
@@ -1,16 +1,24 @@
+from pathlib import Path
+import sys
+
+if __package__ in {None, ""}:
+    sys.path.append(str(Path(__file__).resolve().parents[1]))
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from app.api.routes import router
-from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
+from slowapi import _rate_limit_exceeded_handler
 from slowapi.middleware import SlowAPIMiddleware
+from app.api.routes import router
+from app.utils.rate_limiter import limiter
+
+
+BASE_DIR = Path(__file__).resolve().parents[1]
+STATIC_DIR = BASE_DIR / "static"
 
 
 app = FastAPI(title="Network Tracker")
-# Rate Limiter
-limiter = Limiter(key_func=get_remote_address)
-
 app.state.limiter = limiter
 app.add_exception_handler(
     RateLimitExceeded,
@@ -19,19 +27,16 @@ app.add_exception_handler(
 
 app.add_middleware(SlowAPIMiddleware)
 app.include_router(router, prefix="/api")
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
-
-@app.get("/")
-def home():
-    return {"message": "Domain Intelligence API is running 🚀"}
-
-from fastapi.middleware.cors import CORSMiddleware
+app.mount("/", StaticFiles(directory=str(STATIC_DIR), html=True), name="static")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500",
-                    "http://127.0.0.1:5500/", 
-                   "https://domain-intelligence-system-dis.vercel.app/"],
+    allow_origins=[
+        "http://localhost:5500",
+        "http://127.0.0.1:8000",
+        "http://127.0.0.1:5500/domain-intelligence-system-dis/static/index.html",
+        "https://domain-intelligence-system-dis.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["GET", "POST"],
     allow_headers=["Authorization", "Content-Type"],
